525284587a01bb30dddc84b69501446ab2c851f4
nigel_lirio <nigel_lirio@yahoo.com>
2018-09-09 22:34:49 -0400

    C++ Setup for ME2

---
+++ ME-2/LIRIO_NIGEL_ME2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class matrix{
+  double A,B,C,D;
+  public:
+  get(int i, int j){
+    
+  }
+  set(int i, int j, double x){
+    
+  }
+}
+int main()
+{
+  
+}
