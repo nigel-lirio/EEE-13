b6ee2fcf8e4c567ac73f15710916705ea1116aff
nigel_lirio <nigel_lirio@yahoo.com>
2018-10-07 23:15:22 -0400

    ME-3

---
+++ ME-3/LIRIO_NIGEL_ME3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+int main(){
+  int num;
+  std::string input;
+  cin >> num;
+  int *cities = new int(num);
+  getline(cin, input);
+  
+}
